08a2c26b99f354aadd93ebc2453b7390b73d085a
Seungho Jeong <sh007.jeong@gmail.com>
2022-03-03 16:57:25 +0900

    add: posting like/unlike

--- k-ingstagram/kingstagram/models.py
+++ k-ingstagram/kingstagram/models.py
@@ -14,17 +14,18 @@ class TimeStampedModel(models.Model):
 
 
 class Post(TimeStampedModel):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="my_post_set", on_delete=models.CASCADE)
     photo = models.ImageField(upload_to="kingstagram/%Y/%m/%d")
     caption = models.CharField(max_length=500)
     tag_set = models.ManyToManyField('Tag', blank=True)
     location = models.CharField(max_length=100)
-
-    def get_absolute_url(self):
-        return reverse("", kwargs={"pk": self.pk})
+    like_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_post_set", blank=True)
 
     def __str__(self):
         return self.caption
+
+    def get_absolute_url(self):
+        return reverse("", kwargs={"pk": self.pk})
 
     def extract_tag_list(self):
         tag_name_list = re.findall(r"#([a-zA-Z\dㄱ-힣]+)", self.caption)
@@ -33,6 +34,12 @@ class Post(TimeStampedModel):
             tag, _ = Tag.objects.get_or_create(name=tag_name)
             tag_list.append(tag)
         return tag_list
+
+    def is_like_user(self, user):
+        return self.like_user_set.filter(pk=user.pk).exists()
+
+    class Meta:
+        ordering = ['-id']
 
 
 class Tag(models.Model):
